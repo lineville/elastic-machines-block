aad1b28820ea8506063e386f3ac5a94699a03110
Liam Neville <lineville@github.com>
2022-04-14 15:32:27 -0700

    wip

--- src/blocks/elastic-machines-config-block/index.tsx
+++ src/blocks/elastic-machines-config-block/index.tsx
@@ -1,8 +1,11 @@
 import { FileBlockProps, getLanguageFromFilename } from "@githubnext/utils";
 import "./index.css";
+import yaml from "js-yaml";
 
 export default function (props: FileBlockProps) {
   const { context, content, metadata, onUpdateMetadata } = props;
+  const doc: any = yaml.load(content);
+  console.log(doc);
   const language = Boolean(context.path)
     ? getLanguageFromFilename(context.path)
     : "N/A";
@@ -15,16 +18,27 @@ export default function (props: FileBlockProps) {
         </h3>
       </div>
       <div className="Box-body">
-        Metadata example: this button has been clicked{" "}
-        <button
-          className="btn"
-          onClick={() =>
-            onUpdateMetadata({ number: (metadata.number || 0) + 1 })
-          }
-        >
-          {metadata.number || 0} times
-        </button>
-        <pre className="mt-3 p-3">{content}</pre>
+        <h2>Config</h2>
+
+        <h3>Providers</h3>
+        <br />
+
+        <ul>
+          {doc.map((provider: any) => (
+            <li key={provider.with.virtualMachineScaleSetId}>
+              <h4>{provider.provider}</h4>
+              <ul>
+                {provider.scale.map((scale: any, idx: number) => (
+                  <ul key={idx}>
+                    <li>- Min: {scale.minimumMachines}</li>
+                    <li>- Max: {scale.maximumMachines}</li>
+                  </ul>
+                ))}
+              </ul>
+              <br />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
